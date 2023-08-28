5ad156884ce529c2d13dab2c12349bed445fc728
Camila Marques de Lima <95050431+CamilaLima21@users.noreply.github.com>
2023-08-27 22:52:20 -0300

    Estudos SpringData

--- src/main/java/br/com/alura/spring/data/service/CrudCargoService.java
+++ src/main/java/br/com/alura/spring/data/service/CrudCargoService.java
@@ -9,56 +9,79 @@ import br.com.alura.spring.data.repository.CargoRepository;
 
 @Service
 public class CrudCargoService {
-
+	
 	private Boolean system = true;
 	private final CargoRepository cargoRepository;
-
+	
 	public CrudCargoService(CargoRepository cargoRepository) {
 		this.cargoRepository = cargoRepository;
 	}
 	
 	public void inicial(Scanner scanner) {
 		while(system) {
-			System.out.println("Qual ação de cargo deseja executar? ");
+			System.out.println("Qual ação de cargo deseja executar");
 			System.out.println("0 - Sair");
 			System.out.println("1 - Salvar");
 			System.out.println("2 - Atualizar");
+			System.out.println("3 - Visualizar");
+			System.out.println("4 - Deletar");
 			
 			int action = scanner.nextInt();
 			
-			switch(action) {
+			switch (action) {
 			case 1:
 				salvar(scanner);
 				break;
 			case 2:
 				atualizar(scanner);
 				break;
+			case 3:
+				visualizar();
+				break;
+			case 4:
+				deletar(scanner);
+				break;
 			default:
 				system = false;
 				break;
 			}
+			
 		}
 		
 	}
 	
 	private void salvar(Scanner scanner) {
-		System.out.println("Descrição do cargo: ");
+		System.out.println("Descrição do cargo de trabalho: ");
 		String descricao = scanner.next();
 		Cargo cargo = new Cargo();
 		cargo.setDescricao(descricao);
 		cargoRepository.save(cargo);
-		System.out.println("Registro salvo com sucesso");
+		System.out.println("Registro salvo com sucesso!");
 	}
 	
 	private void atualizar(Scanner scanner) {
-		System.out.println("Informe o ID: ");
+		System.out.println("Id do cargo de trabalho: ");
 		int id = scanner.nextInt();
-		System.out.println("Informe a nova descrição: ");
+		System.out.println("Descrição do cargo de trabalho: ");
 		String descricao = scanner.next();
+		
 		Cargo cargo = new Cargo();
 		cargo.setId(id);
 		cargo.setDescricao(descricao);
 		cargoRepository.save(cargo);
-		System.out.println("Registro salvo com sucesso");
+		System.out.println("Registro atualizado com sucesso!");
 	}
+	
+	private void visualizar() {
+		Iterable<Cargo> cargos = cargoRepository.findAll();
+		cargos.forEach(cargo -> System.out.println(cargo));
+	}
+	
+	private void deletar(Scanner scanner) {
+		System.out.println("Id do cargo de trabalho: ");
+		int id = scanner.nextInt();
+		cargoRepository.deleteById(id);
+		System.out.println("Registro deletado com sucesso!");
+	}
+	
 }
